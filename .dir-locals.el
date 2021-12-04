ad0eae021353edec2d71eaa97eee788551c797d8
wangtianshu <wang@tianshu.me>
2021-12-04 18:33:09 +0800

    change environment name

--- .dir-locals.el
+++ .dir-locals.el
@@ -3,6 +3,6 @@
 
 ((magit-mode . ((eval . (setq-local magit-git-environment
                                     (append magit-git-environment
-                                            '("CONDA_DEFAULT_ENV=template"))))))
+                                            '("CONDA_DEFAULT_ENV=blocking"))))))
  (python-mode . ((lsp-pyright-venv-path . "~/.local/share/conda/envs")
                  (lsp-pyright-stub-path . "~/.local/lib/python-type-stubs"))))
