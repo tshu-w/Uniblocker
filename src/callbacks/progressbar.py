e8c22339f8b5e6dc2e86e9b76beb6583193b7d8d
wangtianshu <wang@tianshu.me>
2022-08-17 13:57:21 +0800

    delete redundant callbacks

--- src/callbacks/progressbar.py
+++
@@ -1,9 +0,0 @@
-from pytorch_lightning.callbacks import TQDMProgressBar
-
-
-class ProgressBar(TQDMProgressBar):
-    def get_metrics(self, trainer, model):
-        # don't show the version number
-        items = super().get_metrics(trainer, model)
-        items.pop("v_num", None)
-        return items
