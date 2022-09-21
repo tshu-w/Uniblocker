1501cc8a6a5e12459fe5c110511944d4b5c2d060
wangtianshu <wang@tianshu.me>
2022-09-21 20:09:35 +0800

    remove byol

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,7 +1,6 @@
-from .byol import BYOL
 from .deepblocker import DeepBlocker
 from .simclr import SimCLR
 from .simcse import SimCSE
 from .simsiam import SimSiam
 
-__all__ = ["DeepBlocker", "SimCSE", "SimCLR", "BYOL", "SimSiam"]
+__all__ = ["DeepBlocker", "SimCSE", "SimCLR", "SimSiam"]
