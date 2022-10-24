b11fd63d718a5ddc37e48de212abf1670164d76e
wangtianshu <wang@tianshu.me>
2022-10-24 15:35:21 +0800

    remove deepmatcher datamodule

--- src/datamodules/__init__.py
+++ src/datamodules/__init__.py
@@ -1,5 +1,4 @@
 from .blocking import Blocking
-from .deepmatcher import DeepMatcher
 from .gittables import GitTables
 
-__all__ = ["Blocking", "DeepMatcher", "GitTables"]
+__all__ = ["Blocking", "GitTables"]
