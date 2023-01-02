629d7736d4ce8ee9650216e8efee2df73aa7f062
wangtianshu <wang@tianshu.me>
2023-01-02 22:28:50 +0800

    add sweep_recordformer.sh

---
+++ scripts/sweep_recordformer.sh
@@ -0,0 +1,1 @@
+python scripts/sweep_blocking.py fit --config_files "[configs/recordformer.yaml]" --batch_sizes "[64]" --ckpt_paths "[results/fit/recordformer/gittables/7d2m6asi/checkpoints/step=8100-AP=0.49128.ckpt]" --gpus_per_trial 1
