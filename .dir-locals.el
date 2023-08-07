cdc1cbd9cf2037eb0bd9e73df7fe143cecc1abc3
Tianshu Wang <wang@tianshu.me>
2023-08-07 11:33:45 +0000

    Fix .dir-locals

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,7 +1,7 @@
 ;;; Directory Local Variables
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((python-mode . ((eval . (setq eglot-workspace-configuration
+((python-base-mode . ((eval . (setq eglot-workspace-configuration
                                `((:python
                                   :venvPath ,(expand-absolute-name "~/.local/share/conda/envs")
                                   :analysis (:diagnosticMode
